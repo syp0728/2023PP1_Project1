82b69d8b80650c4d917325593eb7489515c5cff4
hm215 <114926636+hm215@users.noreply.github.com>
2023-09-12 10:56:03 +0900

    만들었던 데이터의 이름과 불러온 이름이 달랐던 것을 수정

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class WordCRUD implements ICRUD {
     ArrayList<Word> list;
     Scanner s;
-    final String fname = "Dictionary.txt";
+    final String fname = "Dictionary";
 
     WordCRUD(Scanner s) {
         list = new ArrayList<>();
